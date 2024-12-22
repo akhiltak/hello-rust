da64576efa7c61c44b948a5d3b0fb105fb84a544
akhiltak <akhil@significo.com>
2024-12-22 12:42:01 +0530

    generate docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#![crate_name = "hello_rust"]
+
+/// represent a human
+pub struct Person {
+    /// name of the person
+    name: String,
+}
+
+/// impl a Person
+/// learn from: <https://doc.rust-lang.org/rust-by-example/meta/doc.html>
+impl Person {
+    /// Create a person with given name
+
+    pub fn new(name: &str) -> Person {
+        Person {
+            name: name.to_string(),
+        }
+    }
+
+    /// Gives a friendly hello!
+    ///
+    /// Says "Namaste, jai shree [name](Person::name)!" to the `Person` it is called on.
+    pub fn hello(&self) {
+        println!("Namaste, jai shree {}!", self.name)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Namaste, world!");
+    println!("I'm a Rustacean!");
+
+    let ram = Person::new("Ram");
+
+    ram.hello();
+
+    // comment()
 }
